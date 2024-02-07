999b6b06e31f5d77016e71073f626b20986609d8
Yash Jain NUID(002871982) <jain.yash8@northeastern.edu>
2024-02-07 18:50:03 -0500

    first Commit

---
+++ src/configs/config.js
@@ -0,0 +1,37 @@
+
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+// For postgress
+const sequelize = new Sequelize(process.env.DATABASE, process.env.DB_Name, process.env.PASSWORD, {
+  host: process.env.HOST,
+  dialect: 'postgres',
+  logging: false,
+});
+
+// // For postgress
+// const sequelize = new Sequelize('mydb', 'postgres', 'password', {
+//   host: 'localhost',
+//   dialect: 'postgres',
+//   logging: false,
+// });
+
+// for MySQL
+// const sequelize = new Sequelize('mydb', 'root', 'password', {
+//   host: 'localhost',
+//   dialect: 'mysql',
+//   logging: false,
+// });
+
+const db ={}
+db.sequelize=sequelize
+db.Sequelize=Sequelize;
+
+//Models for the user Table
+db.Users= require('../models/userModels')(sequelize,Sequelize);
+
+module.exports = db;
+  
+
+  
+
