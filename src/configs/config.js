f1a60f72cc74b10b96666c061896b6ef160d5c77
Yash Jain NUID(002871982) <jain.yash8@northeastern.edu>
2024-02-15 14:22:17 -0500

    change in config.js

--- src/configs/config.js
+++ src/configs/config.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(
   process.env.DATABASE, //'mydb',
   process.env.HOSTNAME, //'root',
   process.env.PASSWORD,{ //'password', 
-  host: 'localhost',
+  host: process.env.HOST,
   dialect: 'mysql',
   logging: false,
 });
