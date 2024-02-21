c5720cf84a6a9d7955786bb72cda03dd50ccdbab
Yash Jain NUID(002871982) <jain.yash8@northeastern.edu>
2024-02-21 12:54:00 -0500

    change formate in setupfile

--- packer-GCP/setup.sh
+++ packer-GCP/setup.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
+
+echo "sudo yum update"
+sudo yum update
+
 echo "+-------------------------------------------------------------+"
 echo "|                                                             |"
 echo "|                    Install NodeJS and NPM                   |"
 echo "|                                                             |"
 echo "+-------------------------------------------------------------+"
-echo "sudo yum update"
-sudo yum update
-
-
-echo "Node and npm"
 sudo yum install nodejs npm -y
+
+echo "+-------------------------------------------------------------+"
+echo "|                                                             |"
+echo "|                Install mysql-server and service             |"
+echo "|                                                             |"
+echo "+-------------------------------------------------------------+"
 
 sudo yum -y install mysql-server
 sudo systemctl start mysqld.service
@@ -21,13 +26,23 @@ echo "Enabling MySQL Service..."
 sudo systemctl enable mysqld
 
 echo "Installing npm..."
-# sudo yum -y install epel-release
 sudo yum -y install nodejs npm
 
+
+echo "+-------------------------------------------------------------+"
+echo "|                                                             |"
+echo "|                creating user and group                      |"
+echo "|                                                             |"
+echo "+-------------------------------------------------------------+"
 echo "creating user and group"
 sudo groupadd csye6225
 sudo useradd -g csye6225 -d /opt/csye6225 -s /usr/sbin/nologin csye6225
 
+echo "+-------------------------------------------------------------+"
+echo "|                                                             |"
+echo "|                Installing unzip...                          |"
+echo "|                                                             |"
+echo "+-------------------------------------------------------------+"
 echo "Installing unzip..."
 sudo yum install -y unzip
 
@@ -38,6 +53,19 @@ sudo cp -r  webapp.zip /opt/csye6225
 cd /opt/csye6225
 echo "unzip in opt/csye6225"
 sudo unzip webapp.zip
+
+echo "----Checking if the file exists----"
+ls 
+
+echo "+-------------------------------------------------------------+"
+echo "|                                                             |"
+echo "|                    Install Node Modules                     |"
+echo "|                                                             |"
+echo "+-------------------------------------------------------------+"
+echo "cd to webapp to install node modules"
+cd webapp
+
+sudo npm install
 
 # sudo unzip webapp-fork.zip -d ~/webapp-fork
 echo "MySQL and npm installation completed."
