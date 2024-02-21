69916c5ad2f0d06b8978eadc5067b5078e528d30
Yash Jain NUID(002871982) <jain.yash8@northeastern.edu>
2024-02-20 21:56:14 -0500

    Resolving issues in setup.sh

--- packer-GCP/setup.sh
+++ packer-GCP/setup.sh
@@ -2,19 +2,19 @@
 
 sudo yum update
 sudo yum -y install @mysql
-sudo systemctl -y start mysqld.service
+sudo systemctl start mysqld.service
 
-
-# echo "Starting MySQL Service..."
+echo "Starting MySQL Service..."
 sudo systemctl start mysqld
 
-# echo "Enabling MySQL Service..."
+echo "Enabling MySQL Service..."
 sudo systemctl enable mysqld
 
 echo "Installing npm..."
 sudo yum -y install epel-release
 sudo yum -y install nodejs npm
 
+echo "Installing unzip..."
 sudo yum install -y unzip
 
 # sudo unzip webapp-fork.zip -d ~/webapp-fork
