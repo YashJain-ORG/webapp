cf4ef46ab1d86630c2f419c223683ab41c7b69ab
Yash Jain NUID(002871982) <jain.yash8@northeastern.edu>
2024-02-21 12:05:50 -0500

    testing zip and create user

--- packer-GCP/setup.sh
+++ packer-GCP/setup.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-
+echo "+-------------------------------------------------------------+"
+echo "|                                                             |"
+echo "|                    Install NodeJS and NPM                   |"
+echo "|                                                             |"
+echo "+-------------------------------------------------------------+"
+echo "sudo yum update"
 sudo yum update
+
+
+echo "Node and npm"
+sudo yum install nodejs npm -y
+
 sudo yum -y install mysql-server
 sudo systemctl start mysqld.service
 
@@ -11,16 +21,26 @@ echo "Enabling MySQL Service..."
 sudo systemctl enable mysqld
 
 echo "Installing npm..."
-sudo yum -y install epel-release
+# sudo yum -y install epel-release
 sudo yum -y install nodejs npm
+
+echo "creating user and group"
+sudo groupadd csye6225
+sudo useradd -g csye6225 -d /opt/csye6225 -s /usr/sbin/nologin csye6225
 
 echo "Installing unzip..."
 sudo yum install -y unzip
 
+echo "check webapp in home directory"
+ls
+echo "cp webapp to user home directory"
+sudo cp -r  webapp.zip /opt/csye6225
+cd /opt/csye6225
+echo "unzip in opt/csye6225"
+sudo unzip webapp.zip
+
 # sudo unzip webapp-fork.zip -d ~/webapp-fork
 echo "MySQL and npm installation completed."
 
-# Create the user csye6225 with primary group csye6225 and /usr/sbin/nologin shell
-sudo groupadd csye6225
-sudo useradd -s /usr/sbin/nologin -g csye6225 -m csye6225
+
 
