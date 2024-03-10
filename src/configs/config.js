0fb22c7587d52ba3c25b146d9ec2672d88702304
Yash Jain NUID(002871982) <145181114+yashjainNEU@users.noreply.github.com>
2024-03-09 19:41:32 -0500

    Update config.js

--- src/configs/config.js
+++ src/configs/config.js
@@ -3,13 +3,14 @@ require('dotenv').config();
 
 //for MySQL
 const sequelize = new Sequelize(
-  'mydb',
-  'root',
-  '',{ 
-  host: '127.0.0.1',
-  dialect: 'mysql',
-  logging: false,
-});
+  process.env.DB_NAME, // Use the DB_NAME from .env
+  process.env.DB_USER, // Use the DB_USER from .env
+  process.env.DB_PASSWORD, { // Use the DB_PASSWORD from .env
+    host: process.env.DB_HOST, // Use the DB_HOST from .env
+    dialect: process.env.DB_DIALECT, // Use the DB_DIALECT from .env
+    logging: false,
+  }
+);
 
 const db ={}
 db.sequelize=sequelize
