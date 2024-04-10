3fb0613ccabb0d7f031dc7365092c1f88f5acd3d
Yash Jain NUID(002871982) <jain.yash8@northeastern.edu>
2024-04-09 22:12:27 -0400

    Assignment09

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -153,7 +153,7 @@ const searchUser = (req, resp) => {
       resp.status(200).send(data);
       }else{
         logger.warn("User not verified..");
-        resp.status(400).send({ message: 'User not verified' });
+        resp.status(403).send({ message: 'User not verified' });
       }
     }).catch(error => {
       console.error(error);
@@ -228,6 +228,9 @@ const updateUser=(req,resp)=>{
             }).catch(error=>{
               resp.status(500).send(error);
             })  
+        }else{
+          logger.warn("User not verified..");
+          resp.status(403).send({ message: 'User not verified' });
         }
         
       }).catch(error => {
